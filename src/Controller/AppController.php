c8f709a98fdfa3995d53710d24a8e266eb3d0bc7
ryota1116 <ryota1116funa@gmail.com>
2021-01-22 23:30:22 +0900

    [Add]記事へのアクセスの制限を追加

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -45,6 +45,7 @@ class AppController extends Controller
         $this->loadComponent('Flash');
 
         $this->loadComponent('Auth', [
+            'authorize' => 'Controller',
             'authenticate' => [
                 'Form' => [
                     'fields' => [
@@ -57,13 +58,14 @@ class AppController extends Controller
                 'controller' => 'Users',
                 'action' => 'login'
             ],
-            // コントローラーで isAuthorized を使用します
-            'authorize' => ['Controller'],
             // 未認証の場合、直前のページに戻します
             'unauthorizedRedirect' => $this->referer()
         ]);
-        // display アクションを許可して、PagesControllerが引き続き
-        // 動作するようにします。また、読み取り専用のアクションを有効にします。
+        // display アクションを許可して、PagesControllerが引き続き動作するようにします。また、読み取り専用のアクションを有効にします。
         $this->Auth->allow(['display', 'view', 'index']);
+    }
+
+    public function isAuthorized($user) {
+        return false;
     }
 }
